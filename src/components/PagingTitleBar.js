8894446797627e0cb15b1d02df16e76d90c3dc9b
Scott <scrosehart@gmail.com>
2018-12-17 17:20:16 -0800

    trying to fix color flicker on Announcements and Events when clicking on them. scrolling works fine

--- src/components/PagingTitleBar.js
+++ src/components/PagingTitleBar.js
@@ -5,12 +5,24 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 import Touchable from './Touchable';
 
 export default PagingTitleBar = props => {
-  const { pageTitles, scrollEnabled } = props;
-  const titles = pageTitles.map((title, i) => (
-    <Touchable onPress={() => console.log('pressed')} iosType='opacity' key={i}>
-      <Text>{title}</Text>
-    </Touchable>
-  ));
+  const { currentPage, pageTitles, scrollEnabled } = props;
+  const titles = pageTitles.map((title, i) => {
+    let color = currentPage === title ? 'limegreen' : 'white';
+    const handlePress = i === 0
+                      ? props.scrollToBeginning
+                      : i === pageTitles.length - 1
+                      ? props.scrollToEnd
+                      : () => console.log('pressed');
+    const foo = () => {
+      color = 'limegreen';
+      handlePress();
+    };
+    return (
+      <Touchable onPress={foo} iosType='opacity' key={i}>
+        <Text style={[styles.text, {color}]}>{title}</Text>
+      </Touchable>
+    )
+  });
   return (
     <View>
       <ScrollView
@@ -26,11 +38,14 @@ export default PagingTitleBar = props => {
 };
 
 const styles = EStyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
   scrollView: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignContent: 'center',
-    backgroundColor: 'pink',
+    backgroundColor: 'purple',
   },
 });
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Button, ScrollView, Text, View } from 'react-native';
+import { Button, Dimensions, ScrollView, Text, View } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 import AnnouncementsSubScreen from '../sub-screens/AnnouncementsSubScreen';
 import EventsSubScreen from '../sub-screens/EventsSubScreen';
 
 import PagingTitleBar from '../components/PagingTitleBar';
+
+const { height, width } = Dimensions.get('window');
 
 const pages = () => {
   const pages = {
@@ -27,25 +29,68 @@ console.log( getPageTitles );
 console.log( getPages );
 console.log( getPagesSpecifcValue('title') );
 
-export default HomeScreen = props => {
-  return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Hello from HomeScreen</Text>
-      <Button title='open drawer' onPress={() => props.navigation.openDrawer()} />
-      <PagingTitleBar pageTitles={getPageTitles} scrollEnabled={false} />
-      <View style={styles.scrollViewWrap}>
-        <ScrollView
-          horizontal={true}
-          pagingEnabled={true}
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={styles.scrollView}
-        >
-          <AnnouncementsSubScreen />
-          <EventsSubScreen />
-        </ScrollView>
+export default class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 'Announcements',
+    };
+  }
+
+  scrollToBeginning = () => {
+    this.scrollView.scrollTo( {x: 0} );
+    this.setState({currentPage: 'Announcements'});
+  }
+
+  scrollToEnd = () => {
+    this.scrollView.scrollToEnd();
+    this.setState({currentPage: 'Events'});
+  }
+
+  handleScroll = e => {
+    const { x } = e.nativeEvent.contentOffset;
+    const firstPageX = 0;
+    const secondPageX = width;
+    const xPage = x < width / 2 ? firstPageX : secondPageX;
+    const findPage = x => {
+      const page = {
+        [firstPageX]: 'Announcements',
+        [secondPageX]: 'Events',
+      };
+      return page[x];
+    };
+    this.setState({ currentPage: findPage(xPage) });
+  }
+
+  render() {
+    return (
+      <View style={styles.screen}>
+        <Text style={styles.title}>Hello from HomeScreen</Text>
+        <Button title='open drawer' onPress={() => this.props.navigation.openDrawer()} />
+        <PagingTitleBar
+          currentPage={this.state.currentPage}
+          pageTitles={getPageTitles}
+          scrollEnabled={false}
+          scrollToBeginning={this.scrollToBeginning}
+          scrollToEnd={this.scrollToEnd}
+        />
+        <View style={styles.scrollViewWrap}>
+          <ScrollView
+            ref={scrollView => this.scrollView = scrollView}
+            onScroll={e => this.handleScroll(e)}
+            scrollEventThrottle={5}
+            horizontal={true}
+            pagingEnabled={true}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.scrollView}
+          >
+            <AnnouncementsSubScreen />
+            <EventsSubScreen />
+          </ScrollView>
+        </View>
       </View>
-    </View>
-  )
+    );
+  };
 };
 
 const styles = EStyleSheet.create({
