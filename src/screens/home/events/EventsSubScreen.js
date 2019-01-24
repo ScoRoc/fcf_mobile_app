90e126ed8c998904a7303cfcfaa9e451b196b4b5
Scott <scrosehart@gmail.com>
2019-01-24 01:36:35 -0800

    cleaned up eventsSubScreen with new data format....not finished yet

--- src/screens/home/events/EventsSubScreen.js
+++ src/screens/home/events/EventsSubScreen.js
@@ -20,160 +20,6 @@ const month = date.toLocaleString('en-us', {month: 'long'});
 const year = date.toLocaleString('en-us', {year: 'numeric'});
 const { getKeys } = eventKeys();
 
-//////////////////////////////
-const fakeEvents = () => {
-  const fakeEventsObj = {
-    one: {
-      date: '3',
-      title: 'Drinks at Optimism',
-      type: 'social',
-    },
-    two: {
-      date: '8',
-      throughDate: 'Dec 15',
-      title: 'Cancer Drive',
-      type: 'community',
-    },
-    three: {
-      date: '11',
-      title: '5k Run for ABC',
-      type: 'competition',
-    },
-    four: {
-      date: '17',
-      throughDate: 'Dec 22',
-      title: 'Another Comp Event',
-      type: 'competition',
-    },
-    five: {
-      date: '22',
-      title: 'Another Social Event',
-      type: 'social',
-    },
-    six: {
-      date: '26',
-      title: 'Another Community Event',
-      type: 'community',
-    },
-  };
-  const eventTypes = ['social', 'competition', 'community'];
-  return {
-    getAllEvents: (() => fakeEventsObj)(),
-    getAllEventTypes: (() => eventTypes)(),
-    getEventsByTypes: types => Object.entries(fakeEventsObj).filter(entry => Object.values(entry[1]).some(item => types.indexOf(item) >= 0)),
-    getTypeByTitle: title => fakeEventsObj[Object.keys(fakeEventsObj).find(key => fakeEventsObj[key].title === title)].type,
-  };
-};
-const {
-  getAllEvents,
-  getAllEventTypes,
-  getEventsByTypes,
-  getTypeByTitle
-} = fakeEvents();
-
-const monthMap = {
-  jan: { num: 0, title: 'January' },
-  feb: { num: 1, title: 'February' },
-};
-
-const monthEvents = () => {
-  const eventsArr = [
-    {
-      month: 'January',
-      events: [
-        {
-          date: '3',
-          id: 0,
-          title: 'Drinks at Optimism',
-          type: 'social',
-        },
-        {
-          date: '8',
-          id: 1,
-          throughDate: 'Dec 15',
-          title: 'Cancer Drive',
-          type: 'community',
-        },
-        {
-          date: '11',
-          id: 2,
-          title: '5k Run for ABC',
-          type: 'competition',
-        },
-        {
-          date: '17',
-          id: 3,
-          throughDate: 'Dec 22',
-          title: 'Another Comp Event',
-          type: 'competition',
-        },
-        {
-          date: '22',
-          id: 4,
-          title: 'Another Social Event',
-          type: 'social',
-        },
-        {
-          date: '26',
-          id: 5,
-          title: 'Another Community Event',
-          type: 'community',
-        },
-      ],
-    },
-    {
-      month: 'February',
-      events: [
-        {
-          date: '3',
-          id: 0,
-          title: 'Drinks at Optimism',
-          type: 'social',
-        },
-        {
-          date: '8',
-          id: 1,
-          throughDate: 'Dec 15',
-          title: 'Cancer Drive',
-          type: 'community',
-        },
-        {
-          date: '11',
-          id: 2,
-          title: '5k Run for ABC',
-          type: 'competition',
-        },
-        {
-          date: '17',
-          id: 3,
-          throughDate: 'Dec 22',
-          title: 'Another Comp Event',
-          type: 'competition',
-        },
-        {
-          date: '22',
-          id: 4,
-          title: 'Another Social Event',
-          type: 'social',
-        },
-        {
-          date: '26',
-          id: 5,
-          title: 'Another Community Event',
-          type: 'community',
-        },
-      ],
-    }
-  ];
-  return {
-    getAllMonthData: (() => eventsArr)(),
-  }
-};
-const { getAllMonthData } = monthEvents();
-// console.log('month data: ', getAllMonthData)
-// console.log(getAllMonthData[0].events);
-//////////////////////////////
-
 export default class EventsSubScreen extends React.Component {
   constructor(props) {
     super(props);
@@ -202,7 +48,6 @@ export default class EventsSubScreen extends React.Component {
   }
 
   componentDidMount() {
-    this.setState({eventTypes: getAllEventTypes});
     getWithAxios().then(result => {
       // console.log('sortedEvents: ', result.data.sortedEvents);
       this.setState({ events: result.data.sortedEvents });
@@ -212,42 +57,15 @@ export default class EventsSubScreen extends React.Component {
   render() {
     const { eventTypes, removedTypes } = this.state;
     const width = () => EStyleSheet.value('$width');
-    ///////
-    const events = getEventsByTypes(eventTypes).map((event, i) => {
-      const [key, value] = event;
-      const type = getTypeByTitle(value.title);
-      const eventKey = getKeys[type];
-      return (
-        <EventStrip
-          color={eventKey.color()}
-          date={value.date}
-          dateObj={date}
-          key={i}
-          library={eventKey.library}
-          name={eventKey.name}
-          throughDate={value.throughDate}
-          title={value.title}
-        />
-      )
-    });
-    ///////
-    // console.log('events: ', this.state.events);
-    const year2019 = this.state.events ? this.state.events[0] : null;
-    console.log('year2019: ', year2019);
-    const monthsSections2019 = year2019
-                              ? year2019.months.map(month => {
+    const monthsSections = this.state.events
+                              ? this.state.events.map(month => {
                                   return { title: month.month, data: month.events }
                                 })
                               : null;
-    const sectionList = year2019
+    const sectionList = this.state.events
                       ? <SectionList
-                          // sections={[
-                          //   { title: getAllMonthData[0].month, data: getAllMonthData[0].events },
-                          //   { title: getAllMonthData[1].month, data: getAllMonthData[1].events },
-                          // ]}
-                          sections={monthsSections2019}
+                          sections={monthsSections}
                           renderItem={({item}) => {
-                            // console.log('item: ', item)
                             const eventKey = getKeys[item.types[0]];
                             return  (
                                       <EventStrip
@@ -270,46 +88,10 @@ export default class EventsSubScreen extends React.Component {
                           keyExtractor={item => item._id}
                         />
                         : null;
-    console.log('monthsSections2019: ', monthsSections2019);
     return (
       <View style={[styles.screen, {width: width()}]}>
         <EventsKey filterEventTypes={this.filterEventTypes} removedTypes={removedTypes} />
-        <View style={styles.monthWrapper}>
-          {/* <Text style={styles.monthText}>{month} {year}</Text> */}
-        </View>
-        {/* <ScrollView>
-          {events}
-        </ScrollView> */}
         {sectionList}
-        {/* <SectionList
-          // sections={[
-          //   { title: getAllMonthData[0].month, data: getAllMonthData[0].events },
-          //   { title: getAllMonthData[1].month, data: getAllMonthData[1].events },
-          // ]}
-          sections={monthsSections2019}
-          renderItem={({item}) => {
-            // console.log('item: ', item)
-            const eventKey = getKeys[item.type];
-            return  (
-                      <EventStrip
-                        color={eventKey.color()}
-                        date={item.date}
-                        dateObj={date}
-                        library={eventKey.library}
-                        name={eventKey.name}
-                        throughDate={item.throughDate}
-                        title={item.title}
-                      />
-                    );
-          }}
-          renderSectionHeader={({section}) => {
-            // console.log('section: ', section)
-              return  <View style={styles.monthWrapper}>
-                        <Text style={styles.monthText}>{section.title}</Text>
-                      </View>
-          }}
-          keyExtractor={item => item.id}
-        /> */}
       </View>
     );
   }
